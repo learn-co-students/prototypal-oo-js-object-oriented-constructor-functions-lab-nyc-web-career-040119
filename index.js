7036e972af3ef55540462dc6db9734eca2ad1cec
William Sinn <w.h.sinn@gmail.com>
2019-05-23 13:53:34 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function Driver(name, age, experience) {
+    this.name=name;
+    this.age=age;
+    this.experience=experience;
+}
+
+let allison = new Driver("Allison", "16 years", "2 years");
+
+
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+
+let samson = new Scooter(2015, "red", "swoosh");
+
+
+function PickupLocation(address, city){
+  this.address = address;
+  this.city = city;
+}
