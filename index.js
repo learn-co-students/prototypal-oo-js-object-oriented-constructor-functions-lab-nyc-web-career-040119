028f55a287e4f808cb85dad0269620bcd814f2ba
danfyfe <fyfedaniel@gmail.com>
2019-05-23 14:05:16 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+class Scooter {
+  constructor(year,color,model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+}//end of scooter class
+
+
+
+class Driver {
+  constructor(name,age,experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+}// end of Driver
+
+
+class PickupLocation {
+  constructor(address,city) {
+    this.address = address;
+    this.city = city;
+  }
+}//end PickupLocation class
